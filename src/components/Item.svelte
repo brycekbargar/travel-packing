6d4703dd7597bba4f7c994ac655cf97bf6d73e05
bryce <brycekbargar@gmail.com>
2020-12-19 14:00:58 -0600

    Add the item component and model

---
+++ src/components/Item.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    import type { Item } from '../model/Item'
+</script>
+<script>
+    export let item: Item;
+
+    let editing = false;
+</script>
+
+<li>
+    <input type="checkbox"
+        bind:checked="{item.packed}" />
+    {#if editing}
+        <input type="text" 
+            on:blur="{() => editing = false}"
+            bind:value="{item.name}" />
+    {:else}
+        <span 
+            on:click="{() => editing = true}">
+            {item.name}
+        </span>
+    {/if}
+    <button>&#x1F5D1;</button>
+</li>
---
+++ src/model/Item.ts
@@ -0,0 +1,28 @@
+/** Item represents a unique something intended to be packed. */
+export class Item {
+    readonly id: string;
+    name: string;
+    packed: boolean;
+
+    /**
+     * 
+     * @param id The unique id of the Item.
+     * @param name The name of the Item.
+     * @param packed An (optional) value indicating whether the item has been packed.
+     */
+    constructor(id: string, name: string, packed?:boolean) {
+        this.id = id;
+        this.name = name;
+        this.packed = packed == null ? false : packed;
+    }
+}
+
+/** 
+ * A function for checking whether the Item's name is unique.
+ * @param itemName The name of the Item to check.
+ * @param itemId An (optional) id of the Item to allow for an item to keep it's own name.
+ * @returns A value indicating whether the name of the Item is unique.
+ */
+export interface UniqueItemGuarantor {
+    (itemName: string, itemId?: string): boolean;
+}

39e49c0cb7cfd7066541050306b364deb358ca57
bryce <brycekbargar@gmail.com>
2020-12-21 15:56:27 -0600

    Use the inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...require('tailwindcss/defaultTheme').fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},

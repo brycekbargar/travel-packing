9fbeede802884e8011eeb70eab0f171d61a8d776
bryce <brycekbargar@gmail.com>
2020-12-24 15:39:47 -0600

    Configure testing (but dont actually test stuff)

--- src/App.test.ts
+++ src/App.test.ts
@@ -3,9 +3,9 @@ import {expect} from 'chai';
 import App from './App.svelte';
 
 describe('<App>', () => {
-  it('renders learn svelte link', () => {
+  it('renders signup (on the login component)', () => {
     const {getByText} = render(App);
-    const linkElement = getByText(/learn svelte/i);
+    const linkElement = getByText(/signup/i);
     expect(document.body.contains(linkElement));
   });
 });
